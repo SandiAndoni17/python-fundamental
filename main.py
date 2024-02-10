1350d8690d01a3a1ab96e04d7946e5e2221f4642
Sandianoni17 <sandiandoni17@gmail.com>
2024-02-11 06:34:16 +0700

    intial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+# Percabangan
+print('ibu berkata,"beli susu"')
+print('anak berkata,"ok bu"')
+print('anak pergi ke toko susu')
+
+jumlah_susu = 0
+if jumlah_susu > 0:
+
+    print('anak beli satu botol susu')
+else:
+    print('anak tidak jadi beli susu')
+
+print('anak pulang ke rumah')
+print('anak berikan susu ke ibu')
