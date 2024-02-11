f7976ddeaa76bf386f7ae795fa918995893e426a
Sandianoni17 <sandiandoni17@gmail.com>
2024-02-11 10:48:28 +0700

    code

---
+++ perulangan.py
@@ -0,0 +1,9 @@
+book_count =10
+print('Perintah Ibu, "Baca Semua Bukumu"')
+read_book_count = 0
+while read_book_count < book_count:
+    read_book_count +=1
+    print('Budi Telah Membaca {} buah buku' .format(read_book_count))
+
+print('Budi Telah Membaca Semua Buku')
+
