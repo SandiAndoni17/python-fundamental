fcf1b6abcb68ca16988361fbe0b904c35ae94076
Sandianoni17 <sandiandoni17@gmail.com>
2024-02-11 08:36:41 +0700

    code

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ print('ibu berkata,"beli susu"')
 print('anak berkata,"ok bu"')
 print('anak pergi ke toko susu')
 
-jumlah_susu = 0
+jumlah_susu = 123
+jumlah_telur = 456
+print(f'Jumlah susu adalah {jumlah_susu} botol')
+print(f'jumlah telur adalah {jumlah_telur} butir')
 if jumlah_susu > 0:
-
     print('anak beli satu botol susu')
+
 else:
     print('anak tidak jadi beli susu')
+
+if jumlah_telur > 0:
+    print('anak beli 6 butir telur')
+else:
+    print('anak hanya beli satu botol susu')
 
 print('anak pulang ke rumah')
 print('anak berikan susu ke ibu')
